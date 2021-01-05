0c3c731879dd5d1a3440230741ec24e5f3bf8613
Ashish Thakur <sendashishthakur@gmail.com>
2021-01-05 22:49:14 +0530

    array
    added array reversing

---
+++ javaScript/array/myArray.js
@@ -0,0 +1,21 @@
+let arr = [1,2,3,4,5]
+let rarr = []
+
+for(i = 0; i< arr.length;i++){
+    console.log(arr[i])
+}
+
+for(i = 0; i< arr.length; i++){
+    
+    console.log(arr[i])
+}
+
+for(i = arr.length -1 ; i >= 0; i--){
+    rarr.push(arr[i])
+}
+
+
+
+
+console.log(arr)
+console.log(rarr)
