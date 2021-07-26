900ccfd32ae3ea812258ea4c7e971a3c924e9677
AshishThakur10 <sendashishthakur@gmail.com>
2021-07-26 23:27:13 +0530

    stack pop

--- DSA/stack2.js
+++ DSA/stack2.js
@@ -11,6 +11,16 @@ class Stack {
         this.count += 1
         return this.count - 1
     }
+
+    //return and remove top element in stack
+    // return undefined if stack is empty
+    pop(){
+        if(this.count == 0) return undefined
+        let deletItem = this.item[this.count -1]
+        this.count -= 1
+        console.log(`${deletItem} removed`)
+        return deletItem
+    }
 }
 
 const stack = new Stack()
@@ -19,3 +29,7 @@ stack.push(100)
 stack.push(200)
 stack.push(300)
 
+stack.pop()
+stack.pop()
+stack.pop()
+console.log(stack.pop())
