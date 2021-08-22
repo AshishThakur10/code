4c274c4226dc564e0bfb6f8c32152a2a241a950f
AshishThakur10 <sendashishthakur@gmail.com>
2021-08-22 22:30:33 +0530

    push method

---
+++ DSA/array/array.js
@@ -0,0 +1,10 @@
+/* push()
+The push() method is an in-built JavaScript method that is used to add a number, 
+string, object, array, or any value to the Array. 
+You can use the push() function that adds new items to the end of an array and returns the new length.
+*/
+const colors = ['black','dark green']
+console.log('before ' + colors)
+
+colors.push('white')            // push method 
+console.log(colors);
