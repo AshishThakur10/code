82fdfee16645771dfb280610cf95c0307060cc84
AshishThakur10 <sendashishthakur@gmail.com>
2021-09-08 21:44:44 +0530

    updated callback function

--- javaScript/callback/callback.js
+++ javaScript/callback/callback.js
@@ -1,10 +1,10 @@
-function mycallback(sum) {
+function showconsole(sum) {
     console.log(sum)
 }
 
 function calculater(num1,num2,mycallback){
     let sum = num1 + num2
-    mycallback(sum)
+    mycallback(sum)          //running the showconsole function
 }
 
-calculater(3,6,mycallback)
+calculater(3,6,showconsole)  //passing a function as a parameter
